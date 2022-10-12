27193dfd6df71aa6822bd9d179172e0d7f3860e6
Ming Qian <mingjqian@gmail.com>
2022-10-12 13:41:32 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App cards">
+      <div className="one card">
+        <p className="one--text">ONE</p>
+      </div>
+      <div className="two card">
+        <p className="two--text">TWO</p></div>
+      <div className="three card">
+        <p className="three--text">THREE</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
